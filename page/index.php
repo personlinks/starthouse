35386c50775b93df8a3a4326cea06795ce83e85b
Thomas Person <37023811+personlinks@users.noreply.github.com>
2018-03-27 13:46:09 +0100

    Add files via upload
    Web Project for PersonLinks Inc.

---
+++ page/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PersonLinks Business Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>
+			<img src="logo.png" />
+		</h1>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<div class="container">
+			<div class="singlet">
+				<p>
+					<b>PersonLinks Incorporated</b><br/> ......... Commited to making your life brighter.
+				</p>
+			</div>
+			<div class="single">
+				<h2>LaTeX Services</h2>
+				<p>
+					<img src="latex.jpg">
+					LaTeX is a computer program for typesetting documents. Simple documents, which do not contain mathematical formulae or tables may be produced very easily: effectively all one has to do is to type the text straight in (though observing certain rules relating to quotation marks and punctuation dashes). Typesetting mathematics is somewhat more complicated, but even here LaTeX is comparatively straightforward to use when one considers the complexity of some of the formulae that it has to produce and the large number of mathematical symbols which it has to produce.
+				</p>
+				<p>
+					LaTeX (created by L. B. Lamport) is one of a number of ‘dialects’ of TEX, which is known as Plain TEX. It is particularly suited to the production of long articles
+					and books, since it has facilities for the automatic numbering of chapters, sections, theorems, equations etc., and also has facilities for cross-referencing. It is probably one of the most suitable version.
+				</p><br/>
+				<p>
+					We have here a team of experts, specialized in typesetting and production of mass scale LaTeX documents ranging from Mathematics projects to far extents at unbelievable speeds and with little or no errors and thus, no need for fear of editing. Believe us when we say we are the best at this in this area.
+				</p>
+				<p>
+					Our services come at moderate costs and we guarantee a job well done at every transaction that we do. Do not hesitate to contact us and make sure your projects, assignments, etc are guaranteed safe and sound.
+				</p>
+				<button>
+					<a href="#">Request</a>
+				</button>
+			</div>
+			<div class="single">
+				<h2>Data Analysis Services</h2>
+				<p>
+					<img src="data.jpg">
+					Data analysis is a primary component of data mining and Business Intelligence (BI) and is key to gaining the insight that drives business decisions. Organizations and enterprises analyze data from a multitude of sources using Big Data management solutions and customer experience management solutions that utilize data analysis to transform data into actionable insights.
+				</p>
+				<p>
+					Data analysis involves asking questions about what happened, what is happening, and what will happen (predictive analytics). As Junk puts it, “analytics is generally the data crunching, question-answering phase leading up to the decision-making phase in the overall Business Intelligence process.”
+				</p>
+				<p>
+					But, there is so much data available today that data analysis is a challenge. Namely, handling and presenting all of the data are two of the most challenging aspects of data analysis. Traditional architectures and infrastructures are not able to handle the sheer amount of data that is being generated today, and decision makers (particularly students) find it takes longer than anticipated to get actionable insight from the data.
+					Plus, data analysis has also proven to be a necessary requirement for the completeion of most end of year projects and students often face large scale difficulties in validating this aspects.
+				</p>
+				<p>
+					We therefore have a team of specialized Data analysis experts to effectively analyze your data using the best applications which maximize accuracy and minimize chances of errors.
+				</p>
+				<p>
+					Our services come at very moderate rate and a job well done is guaranteed.
+				</p>
+				<button>
+					<a href="#">Request</a>
+				</button>
+			</div>
+			<div class="single">
+				<h2>Documentation - Typing and Production</h2>
+			</div>
+			<div class="single">
+				<h2>Software and Hardware Maintenance</h2>
+				<p>
+					<img src="hardware.jpg">
+					Maintenance services include both hardware maintenance and support services, and network software maintenance and support services.
+				</p>
+				<p>
+					Hardware maintenance and support services are preventive and remedial services that physically repair or optimize hardware, including contract maintenance and per-incident repair. Hardware support also includes online and telephone technical troubleshooting and assistance for setup, and all fee-based hardware warranty upgrades.
+				</p>
+				<p>
+					Sales of all parts are also included, exclusive of parts bundled with maintenance contracts. This segment includes only external customer spending on these services.
+				</p>
+				<p>
+					Software maintenance and support services include long-term and pay-as-you-go (incident-based) support contracts. Software support contracts include remote troubleshooting and support provided via the telephone and online channels, as well as installation assistance and basic usability assistance. In some cases, software support services may include new product installation services, installation of product updates, migrations for major releases of software and other types of proactive or reactive on-site services. Software products and technologies covered under this category include operating systems and infrastructure software.
+				</p>
+				<button>
+					<a href="#">Request</a>
+				</button>
+			</div>
+			<div class="single">
+				<h2>Computer Training Services</h2>
+				<p>
+					<img src="cs-training.png">
+					We provide personalized computer training and consulting within the University of Bamenda area since 2016. Owner Melissa Guzzetta and her staff are accomplished trainers and consultants with real-world business experience in both large and small organizations. Our capabilities are big, but our service is personal, hands-on, and completely invested in giving every student and client the most personalized use of their computer technology.
+				</p>
+				<p>
+					We help large companies train their employees, start-ups get their accounting started right, small businesses improve their process, sales people create better marketing materials, students prepare for the real world, job seekers create callback-worthy resumés, grandmas keep in touch with distant grandchildren – and hundreds of average computer users like you.
+				</p>
+				<button>
+					<a href="#">Request</a>
+				</button>
+			</div>
+			<div class="single">
+				<h2>Web Development</h2>
+				<p>
+					<img src="web.jpg">
+					Web development is a broad term for the work involved in developing a web site for the Internet (World Wide Web) or an intranet (a private network). Web development can range from developing the simplest static single page of plain text to the most complex web-based internet applications (or just 'web apps') electronic businesses, and social network services. A more comprehensive list of tasks to which web development commonly refers, may include web engineering, web design, web content development, client liaison, client-side/server-side scripting, web server and network security configuration, and e-commerce development.
+				</p>
+				<p>
+					 There are three kinds of web development specialization: front-end development, back-end development, and full-stack development. Front-end developers deal with the layout and visuals of a website, while back-end developers deal with the functionality of a website. Back-end developers will program in the functions of a website that will collect data.
+				</p>
+				<p>
+					Since the commercialization of the web, web development has been a growing industry. The growth of this industry is being driven by businesses wishing to use their website to sell products and services to customers.
+				</p>
+				<p>
+					We therefore have specialized web developers for all of these domains to meet up with your needs to maximum satisfacetion. Just request our service if you need one.
+				</p>
+				<button>
+				<a href="#">Request</a>
+				</button>
+			</div>
+			<div class="single">
+				<h2>Software Programming Expertise</h2>
+				<p></p>
+				<p></p>
+
+				<button>
+					<a href="#">Request</a>
+				</button>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<div class="top">
+			<h2> Our Partners</h2>
+			<div class="footerElt">
+				<img src="#"><h4>VITPC</h4><p>www.vitpc.com</p>
+			</div>
+			<div class="footerElt">
+				<img src="#"><h4>tPerson</h4><p>www.tperson.site11.com</p>
+			</div>
+			<div class="footerElt">
+				<img src="#"><h4>Afkanerd Inc.</h4><p>www.afkanerd.com</p>
+			</div>
+		</div>
+		<div class="bottom">PersonLinks Inc. 2018</div>
+	</footer>
+</body>
+</html>
